efa6945223e96d47b1aeafb2a4961b81d61feeb5
João Vitor Lima <jvolima2004@gmail.com>
2023-01-01 23:07:33 -0300

    test: ensure Login start with inputs with required state

--- src/presentation/pages/login/index.spec.tsx
+++ src/presentation/pages/login/index.spec.tsx
@@ -5,10 +5,18 @@ import { Login } from '.'
 describe('Login component', () => {
   it('Should be able to start with initial state', () => {
     const { getByTestId } = render(<Login />)
-    const errorWrap = getByTestId('error-wrap')
-    expect(errorWrap.childElementCount).toBe(0)
+    const formStatus = getByTestId('form-status')
+    expect(formStatus.childElementCount).toBe(0)
 
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('🔴')
+
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.textContent).toBe('🔴')
   })
 })
