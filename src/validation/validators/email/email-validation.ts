4cfecdf292ef46204dee22b1cfacc24b60eb8350
João Vitor Lima <jvolima2004@gmail.com>
2023-01-05 20:47:14 -0300

    feat: ensure EmailValidation returns error if email is invalid

---
+++ src/validation/validators/email/email-validation.ts
@@ -0,0 +1,12 @@
+import { InvalidFieldError } from '@/validation/errors'
+import { FieldValidation } from '@/validation/protocols/field-validation'
+
+export class EmailValidation implements FieldValidation {
+  constructor (
+    readonly field: string
+  ) {}
+
+  validate (value: string): Error {
+    return new InvalidFieldError()
+  }
+}
