121ecf080242bb50714b37e9d0e195324d627105
João Vitor Lima <jvolima2004@gmail.com>
2023-01-05 21:19:09 -0300

    test: ensure MinLengthValidation returns falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -8,4 +8,10 @@ describe('MinLengthValidation', () => {
     const error = sut.validate('123')
     expect(error).toEqual(new InvalidFieldError())
   })
+
+  it('Should be able to return falsy if value is valid', () => {
+    const sut = new MinLengthValidation(faker.database.column(), 5)
+    const error = sut.validate('12345')
+    expect(error).toBeFalsy()
+  })
 })
