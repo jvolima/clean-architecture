d65b7457116c691977383053a0ee5bba3c8a48cc
João Vitor Lima <jvolima2004@gmail.com>
2023-01-11 19:34:26 -0300

    feat: ensure CompareFieldsValidation return error if compare is invalid

---
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -0,0 +1,10 @@
+import { InvalidFieldError } from '@/validation/errors'
+import { FieldValidation } from '@/validation/protocols/field-validation'
+
+export class CompareFieldsValidation implements FieldValidation {
+  constructor (readonly field: string) {}
+
+  validate (value: string): Error {
+    return new InvalidFieldError()
+  }
+}
