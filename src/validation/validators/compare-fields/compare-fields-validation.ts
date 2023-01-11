761edee36e3abdf27f5be52a5d5b01170d26649d
João Vitor Lima <jvolima2004@gmail.com>
2023-01-11 19:43:33 -0300

    feat: ensure CompareFieldsValidation return falsy if compare is valid

--- src/validation/validators/compare-fields/compare-fields-validation.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -2,9 +2,12 @@ import { InvalidFieldError } from '@/validation/errors'
 import { FieldValidation } from '@/validation/protocols/field-validation'
 
 export class CompareFieldsValidation implements FieldValidation {
-  constructor (readonly field: string) {}
+  constructor (
+    readonly field: string,
+    private readonly valueToCompare: string
+  ) {}
 
   validate (value: string): Error {
-    return new InvalidFieldError()
+    return value !== this.valueToCompare ? new InvalidFieldError() : null
   }
 }
