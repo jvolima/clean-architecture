ab509601708ff7d3d28f48dd24997d965234d263
João Vitor Lima <jvolima2004@gmail.com>
2023-01-11 11:48:27 -0300

    refactor data mocks

--- src/data/test/mock-http-post.ts
+++
@@ -1,7 +0,0 @@
-import { faker } from '@faker-js/faker'
-import { HttpPostParams } from '../protocols/http'
-
-export const mockPostRequest = (): HttpPostParams<any> => ({
-  url: faker.internet.url(),
-  body: faker.internet.email()
-})
--- src/data/test/mock-http.ts
+++ src/data/test/mock-http.ts
@@ -1,4 +1,10 @@
-import { HttpPostClient, HttpPostParams, HttpResponse, HttpStatusCode } from '../protocols/http'
+import { HttpPostParams , HttpPostClient, HttpResponse, HttpStatusCode } from '../protocols/http'
+import { faker } from '@faker-js/faker'
+
+export const mockPostRequest = (): HttpPostParams<any> => ({
+  url: faker.internet.url(),
+  body: faker.internet.email()
+})
 
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   url?: string
