21ba37a18d88641b463c89e306c44a8f1439e39d
João Vitor Lima <jvolima2004@gmail.com>
2023-01-12 12:56:19 -0300

    feat: ensure SignUp change name status to error if validation fails

--- src/presentation/pages/signup/index.tsx
+++ src/presentation/pages/signup/index.tsx
@@ -1,22 +1,35 @@
 import { Footer, FormStatus, Input, LoginHeader } from '@/presentation/components'
 import { FormContext } from '@/presentation/contexts/form/form-context'
-import React, { useState } from 'react'
+import { Validation } from '@/presentation/protocols/validation'
+import React, { useEffect, useState } from 'react'
 import styles from './styles.scss'
 
-export function SignUp (): JSX.Element {
-  const [state] = useState({
+type Props = {
+  validation?: Validation
+}
+
+export function SignUp ({ validation }: Props): JSX.Element {
+  const [state, setState] = useState({
     isLoading: false,
-    nameError: 'Campo obrigatório',
+    name: '',
+    nameError: '',
     emailError: 'Campo obrigatório',
     passwordError: 'Campo obrigatório',
     passwordConfirmationError: 'Campo obrigatório',
     mainError: ''
   })
 
+  useEffect(() => {
+    setState({
+      ...state,
+      nameError: validation.validate('name', state.name)
+    })
+  }, [state.name])
+
   return (
     <div className={styles.signup}>
       <LoginHeader />
-      <FormContext.Provider value={{ state }}>
+      <FormContext.Provider value={{ state, setState }}>
         <form className={styles.form}>
           <h2>Criar Conta</h2>
           <Input type="text" name="name" placeholder="Digite seu nome" />
