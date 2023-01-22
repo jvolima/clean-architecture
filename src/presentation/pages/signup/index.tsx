01384daa69aa65b7b22a3b75560bdaa8cd43a4b5
João Vitor Lima <jvolima2004@gmail.com>
2023-01-22 19:09:58 -0300

    feat: ensure SignUp change email status to error if validation fails

--- src/presentation/pages/signup/index.tsx
+++ src/presentation/pages/signup/index.tsx
@@ -13,7 +13,8 @@ export function SignUp ({ validation }: Props): JSX.Element {
     isLoading: false,
     name: '',
     nameError: '',
-    emailError: 'Campo obrigatório',
+    email: '',
+    emailError: '',
     passwordError: 'Campo obrigatório',
     passwordConfirmationError: 'Campo obrigatório',
     mainError: ''
@@ -22,9 +23,10 @@ export function SignUp ({ validation }: Props): JSX.Element {
   useEffect(() => {
     setState({
       ...state,
-      nameError: validation.validate('name', state.name)
+      nameError: validation.validate('name', state.name),
+      emailError: validation.validate('email', state.email)
     })
-  }, [state.name])
+  }, [state.name, state.email])
 
   return (
     <div className={styles.signup}>
