c43a8ca84552cf01ef1d261f3de3e40f6a6feb35
João Vitor Lima <jvolima2004@gmail.com>
2023-01-22 19:31:43 -0300

    test: ensure SignUp enable submit button if form is valid

--- src/presentation/pages/signup/index.spec.tsx
+++ src/presentation/pages/signup/index.spec.tsx
@@ -89,4 +89,13 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'passwordConfirmation')
     Helper.testStatusForField(sut, 'passwordConfirmation')
   })
+
+  it('Should be able to enable submit button if form is valid', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'name')
+    Helper.populateField(sut, 'email')
+    Helper.populateField(sut, 'password')
+    Helper.populateField(sut, 'passwordConfirmation')
+    Helper.testButtonIsDisabled(sut, 'submit', false)
+  })
 })
