7f262a0baf6120b81c6dfe81c3af9aaea293fa1b
João Vitor Lima <jvolima2004@gmail.com>
2023-01-26 10:39:03 -0300

    test: ensure Login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,16 @@
 describe('Login', () => {
-  it('Should be able to load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+
+  it('Should be able to load with correct initial state', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('form-status').should('not.have.descendants')
   })
 })
