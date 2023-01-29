9f973ef473bc9089f7feaa151215d5d719539de8
João Vitor Lima <jvolima2004@gmail.com>
2023-01-29 15:36:33 -0300

    test: ensure SignUp present valid state if form is valid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -31,4 +31,18 @@ describe('SignUp', () => {
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('form-status').should('not.have.descendants')
   })
+
+  it('Should be able to present valid state if form is valid', () => {
+    cy.getByTestId('name').focus().type(faker.name.fullName())
+    FormHelper.testInputStatus('name')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    FormHelper.testInputStatus('email')
+    const password = faker.random.alphaNumeric(5)
+    cy.getByTestId('password').focus().type(password)
+    FormHelper.testInputStatus('password')
+    cy.getByTestId('passwordConfirmation').focus().type(password)
+    FormHelper.testInputStatus('passwordConfirmation')
+    cy.getByTestId('submit').should('not.have.attr', 'disabled')
+    cy.getByTestId('form-status').should('not.have.descendants')
+  })
 })
