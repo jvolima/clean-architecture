2f44e64b7f6406bfa88536d49492ef94cbd70c88
João Vitor Lima <jvolima2004@gmail.com>
2023-02-10 14:49:59 -0300

    test: ensure SurveyItem load icon and day correctly

--- src/presentation/pages/survey-list/components/survey-item/index.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/index.spec.tsx
@@ -12,14 +12,14 @@ const makeSut = (survey: SurveyModel): void => {
 describe('SurveyItem Component', () => {
   it('Should be able to render with correct values', () => {
     const survey = Object.assign(mockSurveyModel(), {
-      didAnswer: true,
-      date: new Date('2023-02-10T00:00:00')
+      didAnswer: false,
+      date: new Date('2019-05-03T00:00:00')
     })
     makeSut(survey)
-    expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp)
+    expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbDown)
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
-    expect(screen.getByTestId('day')).toHaveTextContent('10')
-    expect(screen.getByTestId('month')).toHaveTextContent('fev')
-    expect(screen.getByTestId('year')).toHaveTextContent('2023')
+    expect(screen.getByTestId('day')).toHaveTextContent('03')
+    expect(screen.getByTestId('month')).toHaveTextContent('mai')
+    expect(screen.getByTestId('year')).toHaveTextContent('2019')
   })
 })
