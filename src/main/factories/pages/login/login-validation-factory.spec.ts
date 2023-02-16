178e46558c04f25e9fc7adf80a07edb8bea3aad7
João Vitor Lima <jvolima2004@gmail.com>
2023-02-16 09:08:16 -0300

    refactor: change validation factory to not use builder

--- src/main/factories/pages/login/login-validation-factory.spec.ts
+++ src/main/factories/pages/login/login-validation-factory.spec.ts
@@ -1,12 +1,14 @@
-import { ValidationComposite, ValidationBuilder as Builder } from '@/validation/validators'
+import { ValidationComposite, RequiredFieldValidation, EmailValidation, MinLengthValidation } from '@/validation/validators'
 import { makeLoginValidation } from './login-validation-factory'
 
 describe('LoginValidationFactory', () => {
   it('Should be able to make ValidationComposite with correct validations', () => {
     const composite = makeLoginValidation()
     expect(composite).toEqual(ValidationComposite.build([
-      ...Builder.field('email').required().email().build(),
-      ...Builder.field('password').required().min(5).build()
+      new RequiredFieldValidation('email'),
+      new EmailValidation('email'),
+      new RequiredFieldValidation('password'),
+      new MinLengthValidation('password', 5)
     ]))
   })
 })
--- src/main/factories/pages/signup/signup-validation-factory.spec.ts
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -1,14 +1,18 @@
-import { ValidationComposite, ValidationBuilder as Builder } from '@/validation/validators'
+import { ValidationComposite, RequiredFieldValidation, MinLengthValidation, EmailValidation, CompareFieldsValidation } from '@/validation/validators'
 import { makeSignUpValidation } from './signup-validation-factory'
 
 describe('SignUpValidationFactory', () => {
   it('Should be able to make ValidationComposite with correct validations', () => {
     const composite = makeSignUpValidation()
     expect(composite).toEqual(ValidationComposite.build([
-      ...Builder.field('name').required().min(5).build(),
-      ...Builder.field('email').required().email().build(),
-      ...Builder.field('password').required().min(5).build(),
-      ...Builder.field('passwordConfirmation').required().sameAs('password').build()
+      new RequiredFieldValidation('name'),
+      new MinLengthValidation('name', 5),
+      new RequiredFieldValidation('email'),
+      new EmailValidation('email'),
+      new RequiredFieldValidation('password'),
+      new MinLengthValidation('password', 5),
+      new RequiredFieldValidation('passwordConfirmation'),
+      new CompareFieldsValidation('passwordConfirmation', 'password')
     ]))
   })
 })
