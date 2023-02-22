676b9770096e3ed2eea24f2ecc4d189ec5cd2af9
João Vitor Lima <jvolima2004@gmail.com>
2023-02-22 11:51:36 -0300

    feat: ensure RemoteLoadSurveyResult throws UnexpectedError on 500

--- src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.spec.ts
@@ -39,4 +39,11 @@ describe('RemoteLoadSurveyResult', () => {
     const promise = sut.load()
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  it('Should be able to throw UnexpectedError if HttpGetClient returns 500', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response.statusCode = HttpStatusCode.serverError
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
