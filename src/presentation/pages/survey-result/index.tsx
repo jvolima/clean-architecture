f747af50e7ef7e4e5cb90700bf354647b0bcc477
João Vitor Lima <jvolima2004@gmail.com>
2023-02-25 17:44:53 -0300

    feat: add skeleton with shimmer on SurveyResult

--- src/presentation/pages/survey-result/index.tsx
+++ src/presentation/pages/survey-result/index.tsx
@@ -8,28 +8,32 @@ export function SurveyResult (): JSX.Element {
     <div className={styles.surveyResultWrap}>
       <Header />
       <div className={styles.contentWrap}>
-        <hgroup>
-          <Calendar date={new Date()} className={styles.calendarWrap} />
-          <h2>Qual é seu framework web favorito?</h2>
-        </hgroup>
-        <FlipMove className={styles.answersList}>
-          <li>
-            <img src="https://logospng.org/download/react/logo-react-1024.png" />
-            <span className={styles.answer}>ReactJS</span>
-            <span className={styles.percent}>50%</span>
-          </li>
-          <li className={styles.active}>
-            <img src="https://logospng.org/download/react/logo-react-1024.png" />
-            <span className={styles.answer}>ReactJS</span>
-            <span className={styles.percent}>50%</span>
-          </li>
-          <li>
-            <img src="https://logospng.org/download/react/logo-react-1024.png" />
-            <span className={styles.answer}>ReactJS</span>
-            <span className={styles.percent}>50%</span>
-          </li>
-        </FlipMove>
-        <button>Voltar</button>
+        { false && (
+        <>
+          <hgroup>
+            <Calendar date={new Date()} className={styles.calendarWrap} />
+            <h2>Qual é seu framework web favorito?</h2>
+          </hgroup>
+          <FlipMove className={styles.answersList}>
+            <li>
+              <img src="https://logospng.org/download/react/logo-react-1024.png" />
+              <span className={styles.answer}>ReactJS</span>
+              <span className={styles.percent}>50%</span>
+            </li>
+            <li className={styles.active}>
+              <img src="https://logospng.org/download/react/logo-react-1024.png" />
+              <span className={styles.answer}>ReactJS</span>
+              <span className={styles.percent}>50%</span>
+            </li>
+            <li>
+              <img src="https://logospng.org/download/react/logo-react-1024.png" />
+              <span className={styles.answer}>ReactJS</span>
+              <span className={styles.percent}>50%</span>
+            </li>
+          </FlipMove>
+          <button>Voltar</button>
+        </>
+        )}
         { false && <Loading /> }
       </div>
       <Footer />
