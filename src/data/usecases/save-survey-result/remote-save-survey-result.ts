9f1728311378e19bf115973633f2328091278350
João Vitor Lima <jvolima2004@gmail.com>
2023-03-04 14:58:35 -0300

    feat: ensure RemoteSaveSurveyResult returns SurveyResult on 200

--- src/data/usecases/save-survey-result/remote-save-survey-result.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.ts
@@ -16,8 +16,11 @@ export class RemoteSaveSurveyResult implements SaveSurveyResult {
       body: params
     })
 
+    const remoteSurveyResult = httpResponse.body
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: return null
+      case HttpStatusCode.ok: return Object.assign(remoteSurveyResult, {
+        date: new Date(remoteSurveyResult.date)
+      })
       case HttpStatusCode.forbidden: throw new AccessDeniedError()
       default: throw new UnexpectedError()
     }
