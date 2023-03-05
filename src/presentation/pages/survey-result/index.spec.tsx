834a830745a3c7a2154b16411c5e5c62241fb904
João Vitor Lima <jvolima2004@gmail.com>
2023-03-05 20:55:58 -0300

    test: ensure SurveyResult present new result on answer click

--- src/presentation/pages/survey-result/index.spec.tsx
+++ src/presentation/pages/survey-result/index.spec.tsx
@@ -174,4 +174,37 @@ describe('SurveyResult Component', () => {
       })
     })
   })
+
+  it('Should be able to present SurveyResult data on SaveSurveyResult success', async () => {
+    const saveSurveyResultSpy = new SaveSurveyResultSpy()
+    const surveyResult = Object.assign(mockSurveyResultModel(), {
+      date: new Date('2022-01-15T00:00:00')
+    })
+    saveSurveyResultSpy.surveyResult = surveyResult
+    makeSut({ saveSurveyResultSpy })
+    await waitFor(async () => {
+      const answersWrap = screen.queryAllByTestId('answer-wrap')
+      fireEvent.click(answersWrap[1])
+      await waitFor(() => {
+        expect(screen.getByTestId('day')).toHaveTextContent('15')
+        expect(screen.getByTestId('month')).toHaveTextContent('jan')
+        expect(screen.getByTestId('year')).toHaveTextContent('2022')
+        expect(screen.getByTestId('question')).toHaveTextContent(surveyResult.question)
+        expect(screen.getByTestId('answers').childElementCount).toBe(surveyResult.answers.length)
+        expect(answersWrap[0]).toHaveClass('active')
+        expect(answersWrap[1]).not.toHaveClass('active')
+        const images = screen.queryAllByTestId('image')
+        expect(images[0]).toHaveAttribute('src', surveyResult.answers[0].image)
+        expect(images[0]).toHaveAttribute('alt', surveyResult.answers[0].answer)
+        expect(images[1]).toBeFalsy()
+        const answers = screen.queryAllByTestId('answer')
+        expect(answers[0]).toHaveTextContent(surveyResult.answers[0].answer)
+        expect(answers[1]).toHaveTextContent(surveyResult.answers[1].answer)
+        const percents = screen.queryAllByTestId('percent')
+        expect(percents[0]).toHaveTextContent(`${surveyResult.answers[0].percent}%`)
+        expect(percents[1]).toHaveTextContent(`${surveyResult.answers[1].percent}%`)
+        expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
+      })
+    })
+  })
 })
